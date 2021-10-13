f176ec56771079c975f607548fc9a77502dbd96f
iliayar <iliayar3@gmail.com>
2021-10-13 15:10:34 +0300

    Pretify
    - Add config
    - Move datasets in separate folder

---
+++ utils/config.py
@@ -0,0 +1,7 @@
+import yaml
+
+class Config:
+
+    def __init__(self, filename: str = "config.yaml"):
+        with open(filename) as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
